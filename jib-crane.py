f6ff3f4b4212c2c3de6eb10cb57c6c82cc67a0f5
deepanshupal09 <101573843+deepanshupal09@users.noreply.github.com>
2022-03-17 15:15:49 +0530

    Update jib-crane.py

--- jib-crane.py
+++ jib-crane.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
-
+import time
+import csv
 
 import math
 import turtle
@@ -79,6 +80,8 @@ def calc():
     tie_obs = float(final_tie_reading.get()) - ze_t
     jib_obs = float(final_jib_reading.get()) - ze_c
 
+    global Comp_Trunc, Ten_Trunc, perc_tie, perc_jib
+
     Compression=(Weight*math.sin(math.radians(b)))/(math.sin(math.radians(a)))
     Comp_Trunc = round(Compression, 3)
     Tension=(Weight*math.sin(math.radians(c)))/(math.sin(math.radians(a)))
@@ -105,6 +108,8 @@ def calc():
     print(perc_tie)
     print(perc_jib)
     turtle_diagram()
+    time.sleep(3)
+    turtle.bye()
 
 
 def clear():
@@ -117,42 +122,130 @@ def clear():
     tens_calc.configure(text="Tension calculated: ")
     perc_error_tie.configure(text="Percentage Error Tie: ")
     perc_error_jib.configure(text="Percentage Error Jib: ")
-    turtle.bye()
 
 def reset():
     clear()
     initial_tie.delete(0, END)
+    #f = open("obs.csv", "w", newline='\n') # clearing observation table
+    #writer = csv.writer(f)
+    #writer.writerow(["S.No ", 'Weight', 'Initial Tie Length', 'Initial Jib Length', 'Post Length', 'Final Jib length', 'Final Tie Length', 'Calculated Tie Length', 'Calculated Jib Length', 'Percantage Error Jib', "Percentage Error Jib"])
+    #f.close()
+    s_no=1
+    for item in tree.get_children():
+        tree.delete(item)
     initial_jib.delete(0, END)
     post.delete(0, END)
     ze_tension.delete(0, END)
     ze_compression.delete(0, END)
     weight.delete(0, END)
 
-def show_table():
+def view_table():
 
     obs_table = Toplevel(main_win)
     obs_table.title('Observation Table')
-    obs_table.geometry("800x600")
+    obs_table.geometry("988x266")
     obs_table.configure(background='white')
 
     obs_heading = Label(obs_table, text="Observation Table", font=('Helvatical bold',20), background='white', foreground='blue', anchor=CENTER)
     obs_heading.grid(row=0, column=0)
 
+    # f = open("obs.csv", "r")
+    # reader = csv.reader(f)
+    #
+    # data = list(reader)
+    # row = len(data)-1
+    # col = 11
+    # for i in range(row):
+    #     for j in range(col):
+    #         e = Entry(obs_table,  width=15, fg='black', font=('Helvatical bold',12))
+    #         e.grid(row=i+1, column= j)
+    #         e.insert(END, data[i][j])
+    #         print(i, end=' ')
+    #     print(j)
+    # f.close()
+    global tree
+    columns = ("S.No", 'Weight', 'Initial Tie Length', 'Initial Jib Length', 'Post Length', 'Final Jib Length', 'Final Tie Length',
+                'Calculated Tie Length', 'Calculated Jib Length', 'Percentage Error Jib', "Percentage Error Tie")
+    tree = ttk.Treeview(obs_table, columns=columns, show='headings')
+    tree.heading('S.No', text='S.No')
+    tree.column("S.No", minwidth=0, width=35, stretch=NO)
 
+    tree.heading('Weight', text='Weight')
+    tree.column("Weight", minwidth=0, width=50, stretch=NO)
+
+    tree.heading('Initial Tie Length', text='Initial Tie Length')
+    tree.column("Initial Tie Length", minwidth=0, width=100, stretch=NO)
+
+    tree.heading('Initial Jib Length', text='Initial Jib Length')
+    tree.column("Initial Jib Length", minwidth=0, width=100, stretch=NO)
+
+    tree.heading('Post Length', text='Post Length')
+    tree.column("Post Length", minwidth=0, width=100, stretch=NO)
+
+    tree.heading('Final Jib Length', text='Final Jib Length')
+    tree.column("Final Jib Length", minwidth=0, width=100, stretch=NO)
+
+    tree.heading('Final Tie Length', text='Final Tie Length')
+    tree.column("Final Tie Length", minwidth=0, width=100, stretch=NO)
+
+    tree.heading('Calculated Tie Length', text='Calc. Tie Length')
+    tree.column("Calculated Tie Length", minwidth=0, width=100, stretch=NO)
+
+    tree.heading('Calculated Jib Length', text='Calc. Jib Length')
+    tree.column("Calculated Jib Length", minwidth=0, width=100, stretch=NO)
+
+    tree.heading('Percentage Error Jib', text='% Error Jib')
+    tree.column("Percentage Error Jib", minwidth=0, width=100, stretch=NO)
+
+    tree.heading('Percentage Error Tie', text='% Error Tie')
+    tree.column("Percentage Error Tie", minwidth=0, width=100, stretch=NO)
+
+    # contacts = []
+    # for n in range(1, 100):
+    #     contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+    #
+    # for contact in contacts:
+    #     tree.insert('', END, values=contact)
+
+    tree.grid(row=1, column=0, sticky='nsew')
+    scrollbar = ttk.Scrollbar(main_win, orient=VERTICAL, command=tree.yview)
+    tree.configure(yscroll=scrollbar.set)
+    scrollbar.grid(row=1, column=1, sticky='ns')
+
+def add_to_table():
+
+    global s_no, tree, Comp_Trunc, Ten_Trunc, perc_jib, perc_tie
+    #f = open("obs.csv", "a")
+    #writer = csv.writer(f)
+    data = [s_no, weight.get(), initial_tie.get(), initial_jib.get(), post.get(), final_jib_reading.get(), final_tie_reading.get(), Comp_Trunc, Ten_Trunc, perc_jib, perc_tie]
+    #writer.writerow(data)
+    tree.insert('', END, values=data)
+    #f.close()
+    s_no += 1
+
+
+
+s_no = 1
 
 main_win = Tk()
 main_win.geometry("1024x768")
 main_win.title('Jib Crane Experiment')
 main_win.configure(background='white')
-main_win.resizable = True
 
 #main_frame = ttk.Frame(main_win, padding=10)
-main_heading =Label(main_win, text="Jib Crane Experiment", )
-main_heading.configure(font=('Helvatical bold',30))
+main_heading = ttk.Label(main_win, text="Jib Crane Experiment", )
+main_heading.configure( font=('Helvatical bold',30), padding=0)
 
 jib_crane_img = Canvas(main_win, height=600, width=500)
 img = ImageTk.PhotoImage(Image.open('jib_crane.jpg'))
 jib_crane_img.create_image(10, 10, anchor=NW, image=img)
+
+f = open("obs.csv", "w", newline='\n') # clearing observation table
+writer = csv.writer(f)
+writer.writerow(["S.No ", 'Weight', 'Initial Tie Length', 'Initial Jib Length', 'Post Length', 'Final Jib length', 'Final Tie Length',
+                'Calculated Tie Length', 'Calculated Jib Length', 'Percantage Error Jib', "Percentage Error Jib"])
+f.close()
+
 #btn = tkinter.Button(main_frame, text="Quit", command=main_win.destroy)
 
 #main_heading.grid(row=0)
@@ -160,31 +253,31 @@ jib_crane_img.create_image(10, 10, anchor=NW, image=img)
 #btn.grid(row=1)
 #btn.config(background='white', font='30', padx=10, pady=10)
 
-ze_tension = Entry(main_win, selectbackground="red", font='30', width=8,highlightthickness=0)
-ze_tension_label = Label(main_win, text="Zero error of tension spring balance", font=('Helvatical bold',13))
+ze_tension = Entry(main_win, selectbackground="red", font='30', width=8)
+ze_tension_label = ttk.Label(main_win, text="Zero error of tension spring balance", font=('Helvatical bold',13))
 
-ze_compression = Entry(main_win, selectbackground="red", font='30', width=8,highlightthickness=0)
-ze_compression_label = Label(main_win, text="Zero error of compression spring balance", font=('Helvatical bold',13))
+ze_compression = Entry(main_win, selectbackground="red", font='30', width=8)
+ze_compression_label = ttk.Label(main_win, text="Zero error of compression spring balance", font=('Helvatical bold',13))
 
-initial_tie = Entry(main_win, selectbackground="red", font='30', width=8,highlightthickness=0)
-initial_tie_label =Label(main_win, text="Initial length of tie (cm)",  font=('Helvatical bold',13))
+initial_tie = Entry(main_win, selectbackground="red", font='30', width=8)
+initial_tie_label = ttk.Label(main_win, text="Initial length of tie (cm)",  font=('Helvatical bold',13))
 
-initial_jib = Entry(main_win, selectbackground="red",  font=('Helvatical bold',13), width=8,highlightthickness=0)
-initial_jib_label = Label(main_win, text="Initial length of jib (cm)",  font=('Helvatical bold',13))
+initial_jib = Entry(main_win, selectbackground="red",  font=('Helvatical bold',13), width=8)
+initial_jib_label = ttk.Label(main_win, text="Initial length of jib (cm)",  font=('Helvatical bold',13))
 
-post = Entry(main_win, selectbackground="red",  font=('Helvatical bold',13), width=8,highlightthickness=0)
-post_label = Label(main_win, text="Length of post (cm)",  font=('Helvatical bold',13))
+post = Entry(main_win, selectbackground="red",  font=('Helvatical bold',13), width=8)
+post_label = ttk.Label(main_win, text="Length of post (cm)",  font=('Helvatical bold',13))
 
-ze_compression = Entry(main_win, selectbackground="red",  font=('Helvatical bold',13), width=8,highlightthickness=0)
-ze_compression_label = Label(main_win, text="Zero error of compression spring balance",  font=('Helvatical bold',13))
+ze_compression = Entry(main_win, selectbackground="red",  font=('Helvatical bold',13), width=8)
+ze_compression_label = ttk.Label(main_win, text="Zero error of compression spring balance",  font=('Helvatical bold',13))
 
-weight = Entry(main_win, selectbackground="red",  font=('Helvatical bold',13), width=8, background='white',highlightthickness=0)
+weight = Entry(main_win, selectbackground="red",  font=('Helvatical bold',13), width=8, background='white')
 weight_label = Label(main_win, text="Weight", background='white')
 
-len_AB = Entry(main_win, selectbackground="red", background="white",  font=('Helvatical bold',13), width=8,highlightthickness=0)
+len_AB = Entry(main_win, selectbackground="red", background="white",  font=('Helvatical bold',13), width=8)
 len_AB_label = Label(main_win, text="Final Jib Length", background='white')
 
-len_AC = Entry(main_win, selectbackground="red", font="30", width=8, background="white",highlightthickness=0)
+len_AC = Entry(main_win, selectbackground="red", font="30", width=8, background="white")
 len_AC_label = Label(main_win, text="Final Tie Length", background='white')
 
 perc_error_tie = Label(main_win, text="Percentage Error Tie: ",  font=('Helvatical bold',13))
@@ -194,17 +287,18 @@ compr_calc = Label(main_win, text="Compression calculated: ",  font=('Helvatical
 tens_calc = Label(main_win, text="Tension calculated: ",  font=('Helvatical bold',13))
 
 final_tie_reading_label = Label(main_win, text="Final reading of tie(kgf)",  font=('Helvatical bold',13))
-final_tie_reading = Entry(main_win,selectbackground='red',  font=('Helvatical bold',13), width=8,highlightthickness=0)
+final_tie_reading = Entry(main_win,selectbackground='red',  font=('Helvatical bold',13), width=8)
 
 final_jib_reading_label = Label(main_win, text="Final reading of jib(kgf)",  font=('Helvatical bold',13))
-final_jib_reading = Entry(main_win, selectbackground='red',  font=('Helvatical bold',13), width=8,highlightthickness=0)
+final_jib_reading = Entry(main_win, selectbackground='red',  font=('Helvatical bold',13), width=8)
 
+calculate_button = HoverButton(main_win, padx=10, pady=10,  font=('Helvatical bold',13), background='white', foreground='#1EC600', text='Calculate', command=calc, activeforeground='white', activebackground='#1EC600')
+clear_button = HoverButton(main_win, padx=10, pady=10, font=('Helvatical bold',13), background='white', foreground='red', text='Clear', command=clear, activeforeground='white', activebackground='red')
+reset_button = HoverButton(main_win, padx=10, pady=10, font=('Helvatical bold',13), background='white', foreground='red', text="reset", command=reset, activeforeground='white', activebackground='red')
 
-calculate_button = HoverButton(main_win, padx=10, pady=10,  font=('Helvatical bold',13), background='white', foreground='#1EC600', text='Calculate', command=calc, activeforeground='white', activebackground='#1EC600',highlightthickness=0,borderwidth=0)
-clear_button = HoverButton(main_win, padx=10, pady=10, font=('Helvatical bold',13), background='white', foreground='red', text='Clear', command=clear, activeforeground='white', activebackground='red',highlightthickness=0,borderwidth=0)
-reset_button = HoverButton(main_win, padx=10, pady=10, font=('Helvatical bold',13), background='white', foreground='red', text="reset", command=reset, activeforeground='white', activebackground='red',highlightthickness=0,borderwidth=0)
+view_table = HoverButton(main_win, padx=10, pady=10, font=('Helvatical bold', 13), background='white', foreground='#F06000', text="View Table", command=view_table, activeforeground='white', activebackground='#F06000')
+add_table = HoverButton(main_win, padx=10, pady=10, font=('Helvatical bold', 13), background='white', foreground='#F06000', text="Add to table", command=add_to_table, activeforeground='white', activebackground='#F06000')
 
-obs_btn = HoverButton(main_win, padx=10, pady=10, font=('Helvatical bold',13), background='white', foreground='#F06000', text="Show observation table", command=show_table, activeforeground='white',activebackground='#F06000',highlightthickness=0,borderwidth=0)
 
 
 #ze_tension_label.grid(row=2, column=0)
@@ -224,7 +318,7 @@ initial_tie.configure(background='white')
 initial_tie_label.configure(background='white', foreground='blue')
 
 initial_jib.configure(background='white')
-initial_jib_label.configure(background='#ffffff', foreground='blue')
+initial_jib_label.configure(background='white', foreground='blue')
 
 post.configure(background='white')
 post_label.configure(background='white', foreground='blue')
@@ -287,9 +381,8 @@ final_jib_reading.place(x=210, y=680)
 calculate_button.place(x=550,y=650)
 clear_button.place(x=660, y=650)
 reset_button.place(x=740, y=650)
-obs_btn.place(x=580, y = 705)
-
-
+view_table.place(x=560, y = 705)
+add_table.place(x=680, y=705)
 
 #main_heading.pack()
 #main_frame.pack()
